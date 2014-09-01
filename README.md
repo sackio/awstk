0e31d52ffbfdb6975b890afd83c4d0f87e915a05
Ben Sack <bsack@pushbuild.com>
2014-09-01 12:11:46 -0400

    added ec2 methods, test suite, and documentation

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # awstk
 
-Toolkit for AWS on Node.js
+awstk is a [Node.js](http://nodejs.org) module for creating routines and workflows with [Amazon Web Services](https://aws.amazon.com)'s APIs. Awstk provides a greater level of abstraction and design patterns for templating common tasks and jobs for various AWS tools.
+
+Currently, awstk focuses on S3, EC2, and methods for EC2 instances. Functions include filesystem-like wrappers for S3, scheduled EBS backups, watching reserved and spot instances, and more.
 
 ## Getting Started
 Install the module with: `npm install awstk`
@@ -10,25 +12,52 @@ var awstk = require('awstk')
   , s3 = new awstk.s3({accessKeyId: "YOUR KEY"
                      , secretAccessKey: "YOUR SECRET"
                      , region: "YOUR REGION"
+                     })
+  , ec2 = new awstk.ec2({accessKeyId: "YOUR KEY"
+                     , secretAccessKey: "YOUR SECRET"
+                     , region: "YOUR REGION"
                      });
+  , instance = new awstk.ec2_instance();
 
-/*
-  Some handy S3 methods, similar to Node's FS module
-*/
-s3.fileExists(bucket, key, callback);
-s3.dirExists(bucket, key, callback);
-s3.getFile(bucket, key, callback);
-s3.writeFile(bucket, key, body, callback);
-s3.copyFile(bucket, key, dest\_bucket, dest\_key, callback);
-s3.moveFile(bucket, key, dest\_bucket, dest\_key, callback);
-s3.deleteFiles(bucket, keys, callback);
-s3.readdir(bucket, key, callback);
-s3.rmdir(bucket, key, callback);
-s3.getGzipFile(bucket, key, callback); //returns unzipped body of file
-s3.writeGzipFile(bucket, key, body, callback); //compresses body and writes to S3
-s3.getJSON(bucket, key, callback); //returns object from file's JSON
-s3.writeJSON(bucket, key, json, callback); //stringifies and writes to S3
-```
+## Methods
+All methods accept optional options object and callback as last two arguments (order doesn't matter). If options are not supplied, defaults are used. If callback is not supplied, noop is used.
+
+### S3
+Methods for better using S3 as a filesystem analagous to methods provided by Node's FS module. For a true FUSE-based solution, check out [riofs](https://github.com/skoobe/riofs)
+
+* **fileExists(bucket, key, callback)** - Check for file existence on S3 (callback receives true or false flag)
+* **dirExists(bucket, key, callback)** - Check for directory existence on S3 (callback receives true or false flag). Directory denotes files stored with a shared key prefix.
+* **getFile(bucket, key, options, callback)** - Retrieve a file. Include options.return_buffer to return a buffer, otherwise file is stringified and passed to callback.
+* **writeFile(bucket, key, body, options, callback)** - Save a file. Body is a string of file data. Include options of acl and encoding (default to private and utf8)
+* **copyFile(bucket, key, dest_bucket, dest_key, options, callback)** - Copy file from one bucket to another. Pass optional encoding and acl.
+* **moveFile(bucket, key, dest_bucket, dest_key, options, callback)** - Move file from one bucket to another. Pass optional encoding and acl.
+* **deleteFiles(bucket, keys, callback)** - Delete multiple files from a bucket.
+* **readdir(bucket, keys, callback)** - Return files in a directory. Directory denotes files with a shared key prefix.
+* **rmdir(bucket, key, callback)** - Remove directory. All files with the given key prefix are deleted.
+* **getGzipFile(bucket, key, options, callback)** - Retrieve a gzipped file, inflating it and passing it to callback. Include options.return_buffer to return a buffer, otherwise inflated file is stringified.
+* **writeGzipFile(bucket, key, body, options, callback)** - Gzip data and save it to a file. Body is a string of file data. Include options of acl and encoding (default to private and utf8)
+* **getJSON(bucket, key, options, callback)** - Retrieves stringified JSON and parses it, sending object to callback.
+* **writeJSON(bucket, key, json, options, callback)** - Stringifies an object and saves it to a file.
+
+### EC2
+Methods abstracting multi-step or polling-based routines dealing with EC2 instances, EBS volumes, and snapshots.
+
+* **createExpiringSnapshot(volume_id, days_to_retain, options, callback)** - Create a snapshot of an EBS volume, tagged to expire after days_to_retain. Used in tandem with deleteExpiredSnapshots.
+* **deleteExpiredSnapshots(callback)** - Delete expiring EBS snapshots which have expired.
+* **watchInstance(instance_id, options, callback)** - Watch an instance, triggering callback when it reaches a given state. Pass options.event to specify a state (defaults to instanceStopped, also accepts instanceRunning or instanceTerminated)
+* **watchSpotPrices(request, handler, stopper, options, callback)** - Watch spot prices, passing the result to handler, and stopping watch when stopper is true. Request is used to optionally filter spot prices returned
+* **watchSpotRequests(request_ids, handler, stopper, options, callback)** - Watch spot requests specified as request_ids, passing the result to handler, and stopping watch when stopper is true.
+* **copyVolume(volume_id, new_volume_data, options, callback)** - Copy a volume to a new volume. new_volume_data is used to specify additional settings for new volume. Pass options.new_instance to attach new volume to new_instance, including options.device as device exposed to instance. Include option.destination_region and options.source_region to copy volume to different region.
+* **swapVolume(volume_id, instance_id, options, callback)** - Swap a volume from one instance to a new instance with instance_id. Include options.device as device exposed to instance (defaults to /dev/sdy).
+
+### EC2 Instance
+Methods for getting instance-specific data on an EC2 instance
+
+* **getUserData(property, callback)** - Get an EC2 instances user-data property
+* **getMetaData(property, callback)** - Get an EC2 instances meta-data property
+
+## Scripts & Plugins
+Future versions will include commandline scripts and module for further abstracting AWS routines
 
 ## License
 Copyright (c) 2014 Ben Sack  
--- test/awstk_test.js
+++ test/awstk_test.js
@@ -3,11 +3,13 @@
 var Awstk = require('../lib/awstk.js')
   , Optionall = require('optionall')
   , Async = require('async')
+  , Moment = require('moment')
   , Belt = require('jsbelt')
   , _ = require('underscore')
   , O = new Optionall()
-  , S3 = new Awstk.s3(O.aws);
-
+  , S3 = new Awstk.s3(O.aws)
+  , EC2 = new Awstk.ec2(_.extend({}, O.aws, {'region': 'us-west-1'}))
+;
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -47,7 +49,7 @@ exports['aws'] = {
         return S3.writeFile(globals.bucket, globals.dir + '/' +globals.doc, globals.body, cb);
       }
     , function(cb){
-        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(globals.get_body === globals.body);
@@ -59,7 +61,7 @@ exports['aws'] = {
         return S3.writeFile(globals.bucket, globals.dir + '/' +globals.doc, globals.body, cb);
       }
     , function(cb){
-        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(globals.get_body === globals.body);
@@ -187,7 +189,7 @@ exports['aws'] = {
         }, cb);
       }
     , function(cb){
-        return S3.readdir(globals.bucket, globals.dir, Belt.callset(cb, globals, 'get_dir'));
+        return S3.readdir(globals.bucket, globals.dir, Belt.callset(cb, globals, 'get_dir', 1, 0));
       }
     , function(cb){
         test.ok(Belt.deepEqual(_.map(globals.counters, function(c){ return globals.doc + '.' + c; }), globals.get_dir));
@@ -207,7 +209,7 @@ exports['aws'] = {
         }, cb);
       }
     , function(cb){
-        return S3.readdir(globals.bucket, globals.dir + '/deep/directory/testing/', Belt.callset(cb, globals, 'get_dir'));
+        return S3.readdir(globals.bucket, globals.dir + '/deep/directory/testing/', Belt.callset(cb, globals, 'get_dir', 1, 0));
       }
     , function(cb){
         test.ok(Belt.deepEqual(_.map(globals.counters, function(c){ return globals.doc + '.' + c; }), globals.get_dir));
@@ -278,14 +280,14 @@ exports['aws'] = {
         return S3.writeGzipFile(globals.bucket, globals.dir + '/' +globals.doc, globals.body, cb);
       }
     , function(cb){
-        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(globals.get_body !== globals.body);
         return cb();
       }
     , function(cb){
-        return S3.getGzipFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getGzipFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(globals.get_body === globals.body);
@@ -301,14 +303,14 @@ exports['aws'] = {
         return S3.writeJSON(globals.bucket, globals.dir + '/' +globals.doc, globals.body, cb);
       }
     , function(cb){
-        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getFile(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(!Belt.deepEqual(globals.get_body, globals.body));
         return cb();
       }
     , function(cb){
-        return S3.getJSON(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body'));
+        return S3.getJSON(globals.bucket, globals.dir + '/' +globals.doc, Belt.callset(cb, globals, 'get_body', 1, 0));
       }
     , function(cb){
         test.ok(Belt.deepEqual(globals.get_body, globals.body));
@@ -327,4 +329,277 @@ exports['aws'] = {
       return test.done();
     });
   },
+  'ec2': function(test) {
+    var globals = {
+      'availability_zone': 'us-west-1b'
+    , 'source_region': 'us-west-1'
+    , 'ami_id': 'ami-f1fdfeb4'
+    , 'instance_type': 't1.micro'
+    };
+
+    return Async.waterfall([
+      //create expiring snapshot
+      function(cb){
+        console.log('Setup volume to play with');
+        return EC2._API.createVolume({'Size': 10, 'AvailabilityZone': globals.availability_zone}, Belt.callset(cb, globals, 'volume', 1, 0));
+      }
+    , function(cb){
+        console.log('Waiting for volume [%s] to be available', globals.volume.VolumeId);
+        test.ok(globals.volume.VolumeId);
+        return EC2.waitFor('volumeAvailable', {'VolumeIds': Belt.toArray(globals.volume.VolumeId)}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Create expiring snapshot');
+        return EC2.createExpiringSnapshot(globals.volume.VolumeId, 30, Belt.callset(cb, globals, 'snapshot', 1, 0));
+      }
+    , function(cb){
+        console.log('Retrieving snapshot');
+        return EC2._API.describeSnapshots({'SnapshotIds': Belt.toArray(globals.snapshot)}, Belt.callset(cb, globals, 'retrieved_snapshot'));
+      }
+    , function(cb){
+        console.log('Asserting expiring snapshot was created');
+
+        var snapshot =  Belt._get(globals, 'retrieved_snapshot.Snapshots.0');
+        test.ok(snapshot);
+        test.ok(snapshot.SnapshotId === globals.snapshot);
+        test.ok(snapshot.State === 'completed');
+
+        var tags = EC2.tag_obj(snapshot.Tags);
+
+        test.ok(tags.Volume === globals.volume.VolumeId);
+        test.ok(tags.Retain === '30:days');
+        test.ok(tags.Expires && Moment(tags.Expires, 'X').fromNow(true) === 'a month');
+
+        return cb();
+      }
+    , function(cb){
+        console.log('Deleting snapshot');
+        return EC2._API.deleteSnapshot({'SnapshotId': globals.snapshot}, Belt.callwrap(cb, 0));
+      }
+
+      //delete expiring snapshots
+    , function(cb){
+        globals.expires = Moment().subtract(90, 'days');
+
+        return EC2._API.createSnapshot({'VolumeId': globals.volume.VolumeId
+          , 'Description': globals.volume.VolumeId + ' BACKUP (Expires ' + globals.expires.format('MM-DD-YYYY')
+          + ' | Retained 20 days)'}
+        , Belt.callset(cb, globals, 'response', 1, 0));
+      }
+    , function(cb){
+        globals.snapshot = Belt._get(globals, 'response.SnapshotId');
+        return cb(!globals.snapshot ? new Error('Snapshot not created') : null);
+      }
+    , function(cb){
+        return EC2._API.createTags({'Resources': Belt.toArray(globals.snapshot), 'Tags': 
+            [ {'Key': 'Expires', 'Value': Belt._call(globals, 'expires.format', 'X')}
+            , {'Key': 'Retain', 'Value': '20:days'}
+            , {'Key': 'Volume', 'Value': globals.volume.VolumeId}
+            , {'Key': 'Name', 'Value': globals.volume.VolumeId + ' BACKUP (Expires ' + globals.expires.format('MM-DD-YYYY')}
+            ]}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+       return EC2.waitFor('snapshotCompleted', {'SnapshotIds': Belt.toArray(globals.snapshot)}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Create expiring snapshot');
+        return EC2.createExpiringSnapshot(globals.volume.VolumeId, 30, Belt.callset(cb, globals, 'snapshot_b', 1, 0));
+      }
+    , function(cb){
+        console.log('Asserting both snapshots were created');
+        return EC2._API.describeSnapshots({'SnapshotIds': [globals.snapshot, globals.snapshot_b]}, Belt.callset(cb, globals, 'retrieved_snapshots', 1, 0));
+      }
+    , function(cb){
+        test.ok(globals.retrieved_snapshots.Snapshots.length === 2);
+        return cb();
+      }
+    , function(cb){
+        console.log('Deleting expired snapshots');
+        return EC2.deleteExpiredSnapshots(Belt.callwrap(cb, 0));
+      }
+
+    , function(cb){
+        console.log('Asserting both snapshots were created');
+        return EC2._API.describeSnapshots({'Filters': [{Name: 'tag-key', Values: ['Expires']}]}, Belt.callset(cb, globals, 'retrieved_snapshots', 1, 0));
+      }
+    , function(cb){
+        test.ok(globals.retrieved_snapshots.Snapshots.length === 1);
+        test.ok(globals.retrieved_snapshots.Snapshots[0].SnapshotId === globals.snapshot_b);
+        return cb();
+      }
+    , function(cb){
+        console.log('Deleting snapshot');
+        return EC2._API.deleteSnapshot({'SnapshotId': globals.snapshot_b}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Deleting volume');
+        return EC2._API.deleteVolume({'VolumeId': globals.volume.VolumeId}, Belt.callwrap(cb, 0));
+      }
+
+      //watchInstances
+    , function(cb){
+        console.log('Creating instance to watch');
+        return EC2._API.runInstances({'ImageId': globals.ami_id, 'MinCount': 1, 'MaxCount': 1, 'InstanceType': globals.instance_type}
+               , Belt.callset(cb, globals, 'instances', 1, 0));
+      }
+    , function(cb){
+        console.log('Asserting instance was created');
+        globals.instance_id = Belt._get(globals, 'instances.Instances.0.InstanceId');
+        test.ok(typeof globals.instance_id !== 'undefined');
+        return cb();
+      }
+    , function(cb){
+        console.log('Watching instance until it is started');
+        return EC2.watchInstance(globals.instance_id, {'event': 'instanceRunning'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Instance started and watch was triggered');
+        return EC2._API.describeInstances({'InstanceIds': [globals.instance_id]}, Belt.callset(cb, globals, 'instance_status', 1, 0));
+      }
+    , function(cb){
+        console.log('Asserting instance is running');
+        var instance = Belt._get(globals, 'instance_status.Reservations.0.Instances.0');
+        test.ok(instance && instance.InstanceId === globals.instance_id && instance.State.Name === 'running');
+        return cb();
+      }
+    , function(cb){
+        console.log('Stopping instance');
+        EC2.watchInstance(globals.instance_id, Belt.callwrap(cb, 0));
+        return EC2._API.stopInstances({'InstanceIds': [globals.instance_id]}, Belt.noop);
+      }
+    , function(cb){
+        console.log('Instance stopped and watch was triggered');
+        return EC2._API.describeInstances({'InstanceIds': [globals.instance_id]}, Belt.callset(cb, globals, 'instance_status', 1, 0));
+      }
+    , function(cb){
+        console.log('Asserting instance was stopped');
+        var instance = Belt._get(globals, 'instance_status.Reservations.0.Instances.0');
+        test.ok(instance && instance.InstanceId === globals.instance_id && instance.State.Name === 'stopped');
+        return cb();
+      }
+    , function(cb){
+        console.log('Terminating instance');
+        EC2.watchInstance(globals.instance_id, {'event': 'instanceTerminated'}, Belt.callwrap(cb, 0));
+        return EC2._API.terminateInstances({'InstanceIds': [globals.instance_id]}, Belt.noop);
+      }
+    , function(cb){
+        console.log('Instance terminated and watch was triggered');
+        test.ok(true);
+        return cb();
+      }
+
+      //watchSpotPrices
+
+      //watchSpotRequests
+
+      //copyVolume
+    , function(cb){
+        console.log('Creating instances');
+        return EC2._API.runInstances({'ImageId': globals.ami_id, 'MinCount': 2, 'MaxCount': 2, 'InstanceType': globals.instance_type
+               , 'Placement': {'AvailabilityZone': globals.availability_zone}}
+               , Belt.callset(cb, globals, 'instances', 1, 0));
+      }
+    , function(cb){
+        console.log('Asserting instances were created');
+        globals.instance_id_a = Belt._get(globals, 'instances.Instances.0.InstanceId');
+        globals.instance_id_b = Belt._get(globals, 'instances.Instances.1.InstanceId');
+        globals.instance_zone_a = Belt._get(globals, 'instances.Instances.0.Placement.AvailabilityZone');
+        globals.instance_zone_b = Belt._get(globals, 'instances.Instances.1.Placement.AvailabilityZone');
+        test.ok(typeof globals.instance_id_a !== 'undefined');
+        test.ok(typeof globals.instance_id_b !== 'undefined');
+        return cb();
+      }
+    , function(cb){
+        console.log('Watching instances until they are started');
+        return EC2.watchInstance(globals.instance_id_a, {'event': 'instanceRunning'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        return EC2.watchInstance(globals.instance_id_b, {'event': 'instanceRunning'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Setup volumes a');
+        return EC2._API.createVolume({'Size': 10, 'AvailabilityZone': globals.instance_zone_a}, Belt.callset(cb, globals, 'volume_a', 1, 0));
+      }
+    , function(cb){
+        console.log('Waiting for volume [%s] to be available', globals.volume_a.VolumeId);
+        test.ok(globals.volume_a.VolumeId);
+        return EC2.waitFor('volumeAvailable', {'VolumeIds': Belt.toArray(globals.volume_a.VolumeId)}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Attaching volume to instance A');
+        return EC2._API.attachVolume({'VolumeId': globals.volume_a.VolumeId, 'InstanceId': globals.instance_id_a, 'Device': '/dev/sdz'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Waiting for volume [%s] to be in use', globals.volume_a.VolumeId);
+        return EC2.waitFor('volumeInUse', {'VolumeIds': Belt.toArray(globals.volume_a.VolumeId)}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Copying volume to instance B');
+        return EC2.copyVolume(globals.volume_a.VolumeId, {'AvailabilityZone': globals.instance_zone_b}, {'new_instance': globals.instance_id_b}
+               , Belt.callset(cb, globals, 'volume_b', 1, 0));
+      }
+    , function(cb){
+        test.ok(globals.volume_b);
+        return cb();
+      }
+    , function(cb){
+        return EC2._API.describeInstances({'InstanceIds': [globals.instance_id_b]}, Belt.callset(cb, globals, 'instance_b', 1, 0));
+      }
+    , function(cb){
+        globals.instance_b = Belt._get(globals, 'instance_b.Reservations.0.Instances.0');
+        test.ok(globals.instance_b.BlockDeviceMappings[1].Ebs.VolumeId === globals.volume_b);
+        return cb();
+      }
+    , function(cb){
+        console.log('Swapping volume');
+        return EC2.swapVolume(globals.volume_a.VolumeId, globals.instance_id_b, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Asserting volume was swapped');
+        return EC2._API.describeInstances({'InstanceIds': [globals.instance_id_b]}, Belt.callset(cb, globals, 'instance_b', 1, 0));
+      }
+    , function(cb){
+        globals.instance_b = Belt._get(globals, 'instance_b.Reservations.0.Instances.0');
+        test.ok(globals.instance_b.BlockDeviceMappings.length === 3);
+        return cb();
+      }
+    , function(cb){
+        return EC2._API.describeInstances({'InstanceIds': [globals.instance_id_a]}, Belt.callset(cb, globals, 'instance_a', 1, 0));
+      }
+    , function(cb){
+        globals.instance_a = Belt._get(globals, 'instance_a.Reservations.0.Instances.0');
+        test.ok(globals.instance_a.BlockDeviceMappings.length === 1);
+        return cb();
+      }
+    , function(cb){
+        console.log('Terminating instance');
+        return EC2._API.terminateInstances({'InstanceIds': [globals.instance_id_a, globals.instance_id_b]}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Watching instances until they are terminated');
+        return EC2.watchInstance(globals.instance_id_a, {'event': 'instanceTerminated'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        return EC2.watchInstance(globals.instance_id_b, {'event': 'instanceTerminated'}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        console.log('Detaching volumes');
+        return EC2._API.detachVolume({'VolumeId': globals.volume_a.VolumeId}, Belt.callwrap(cb));
+      }
+    , function(cb){
+        return EC2._API.detachVolume({'VolumeId': globals.volume_b}, Belt.callwrap(cb));
+      }
+    , function(cb){
+        console.log('Deleting volumes');
+        return EC2._API.deleteVolume({'VolumeId': globals.volume_a.VolumeId}, Belt.callwrap(cb, 0));
+      }
+    , function(cb){
+        return EC2._API.deleteVolume({'VolumeId': globals.volume_b}, Belt.callwrap(cb, 0));
+      }
+    ], function(err){
+      if (err) console.error(err);
+      test.ok(!err);
+      return test.done();
+    });
+  }
 };
